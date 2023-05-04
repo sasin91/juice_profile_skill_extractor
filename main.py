15f91fa2a3cc588703b171678df7e61d568d0da3
sasin91 <jonas.kerwin.hansen@gmail.com>
2023-05-04 13:08:34 +0200

    test paragraph demo

--- main.py
+++ main.py
@@ -12,7 +12,19 @@ filename = sys.argv[1]
 if (filename == ''):
     raise RuntimeError('No filename provided')
 
-input = "Jeg er en udpræget teamplayer med 20 års erfaring fra den digitale verden. Jeg er god til mennesker og brænder for innovation, forandring og bæredygtighed. Min drøm er at gøre en forskel for jer."
+# input = "Jeg er en udpræget teamplayer med 20 års erfaring fra den digitale verden. Jeg er god til mennesker og brænder for innovation, forandring og bæredygtighed. Min drøm er at gøre en forskel for jer."
+
+input = """"
+As a web programmer with nine years of experience using PHP and JavaScript, I have developed a deep understanding of web development and the technologies used in this field. 
+
+Over the years, I have honed my skills in developing robust and scalable web applications using PHP and JavaScript frameworks. 
+
+I am proficient in developing custom web applications as well as integrating third-party applications into existing ones. 
+
+My proficiency in PHP and JavaScript has enabled me to create dynamic and interactive user interfaces that provide an unparalleled user experience. Additionally, I am adept at debugging and troubleshooting complex issues in web applications, ensuring that they run smoothly and efficiently.
+
+Overall, my experience as a web programmer has given me the knowledge and skills necessary to create high-quality web applications that meet the needs of clients and users alike.    
+"""
 
 # init params of skill extractor
 
@@ -21,8 +33,8 @@ input = "Jeg er en udpræget teamplayer med 20 års erfaring fra den digitale ve
 #nlp = load_spacy_model()
 
 # I'm not sure if english web is better than danish news model
-nlp = spacy.load("da_core_news_lg")
-# nlp = spacy.load("en_core_web_lg")
+# nlp = spacy.load("da_core_news_lg")
+nlp = spacy.load("en_core_web_lg")
 
 # init skill extractor
 skill_extractor = SkillExtractor(nlp, SKILL_DB, PhraseMatcher)
